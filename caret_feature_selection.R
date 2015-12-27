78e114ba37603c3c2c178638458732fde5f1115b
himalayajung <himalaya.jung@gmail.com>
2015-12-27 16:53:06 -0500

    Update caret_feature_selection.R

--- caret_feature_selection.R
+++ caret_feature_selection.R
@@ -1,3 +1,5 @@
+# recursive feature elimination using caret
+
 require(caret)
 require(randomForest)
 require(doParallel)
@@ -8,26 +10,26 @@ source('../Rfunctions.R') # R helper functions
 ## Settings
 set.seed(123)
 # brain morphometric properties
-features=list('_volume','_area','_thickness$','_thicknessstd','_foldind','_meancurv','_gauscurv','_all')
+features = list('_volume', '_area', '_thickness$', '_thicknessstd', '_foldind', '_meancurv', '_gauscurv', '_all')
 # features=list('_volume')
-scale_by_TIV=1 # should you normalize the volume features by TIV??
+scale_by_TIV = 1 # should you normalize the volume features by TIV??
 data_splitting=0
 
 ## Data
-df=read.csv('../FS.csv',row.names=1)
-df_imputed=read.csv('../FS_imputed.csv',row.names=1)
+df = read.csv('../FS.csv',row.names=1)
+df_imputed = read.csv('../FS_imputed.csv',row.names=1)
 
-df=df[df$sex==1,] # select only male subjects
-df_imputed=df_imputed[df_imputed$sex==1,]
-df$ADOS=df_imputed$ADOS
-df$control=as.factor(df$control)
+df = df[df$sex == 1,] # select only male subjects
+df_imputed = df_imputed[df_imputed$sex == 1,]
+df$ADOS = df_imputed$ADOS
+df$control = as.factor(df$control)
 
-df=df[df$site=="KKI",] # choose individual site if necessary
-df_imputed=df_imputed[df_imputed$site=="KKI",]
-phenotype=df_imputed[c("age","sex","VIQ","PIQ","FIQ")]
+df = df[df$site=="KKI",] # choose individual site if necessary
+df_imputed = df_imputed[df_imputed$site == "KKI",]
+phenotype = df_imputed[c("age","sex","VIQ","PIQ","FIQ")]
 
 ## setup parallel backend to use multiple processors
-n_cluster=22
+n_cluster = 22
 cl = makeCluster(n_cluster)
 registerDoParallel(cl)
 # Loop along the features
@@ -35,17 +37,17 @@ for (feature in features){
   
   print(paste('************************',feature,'************************'))
   # creating feature specific data
-  if (feature=="_all") {
+  if (feature == "_all") {
     d = df[11:length(df)]
     col_vol = grep("_volume",names(d))  
     TIV = df$EstimatedTotalIntraCranialVol_volume
-    d[col_vol] = as.data.frame(sweep(data.matrix(d[col_vol]),1,TIV,'/'))
-    d=d[-length(d)]
+    d[col_vol] = as.data.frame(sweep(data.matrix(d[col_vol]), 1, TIV, '/'))
+    d = d[-length(d)]
   } else{
-    d=df[grep(feature,names(df))]    }
+    d = df[grep(feature,names(df))]    }
     
     #normalize by TIV
-  if (feature=="_volume" && scale_by_TIV==1){
+  if (feature == "_volume" && scale_by_TIV == 1){
     TIV = d$EstimatedTotalIntraCranialVol_volume
     d = as.data.frame(sweep(data.matrix(d),1,TIV,'/'))
     d = d[-length(d)]}
@@ -64,7 +66,7 @@ for (feature in features){
   #             if(length(comboInfo$remove)>0){
   #               d=d[,-comboInfo$remove]}
   ## Data Splitting
-  if(data_splitting==1){
+  if(data_splitting == 1){
     trainIndex  =  createDataPartition(as.factor(df$control), p=0.8,list = FALSE) # as.factor makes sure that the subjects are evenly sampled from each ADOS score
     Train = d[trainIndex,]
     Test = d[-trainIndex,]
